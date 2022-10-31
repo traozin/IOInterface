dbf278127d11cf83937cff76af760a606c906516
Ozenilson Cruz <59063314+ozenilsoncruz@users.noreply.github.com>
2022-10-31 15:30:02 -0300

    correcoes no dispĺaylcd

--- libs/map.s
+++ libs/map.s
@@ -1,154 +1,279 @@
-/* map.s */
+/* display_lcd.s */
+.include "map.s"
 
-@ Constantes importantes para o programa
-.equ clrregoffset, 40
-.equ setregoffset, 28
-.equ map_shared, 1
-.equ sys_open, 5
-.equ sys_map, 192
-.equ nano_sleep, 162
-.equ prot_read, 1
-.equ prot_write, 2
-.equ pagelen, 4096
-.equ level, 52
- 
+
+.global clearDisplay
+.global entryModeSet
+.global initDisplay
+.global write_lcd
+
 
 /*======================================================
-        Funcao de espara
+        Chama a saida dos pinos do display
   ======================================================
-        Entradas:
-                segundos: segundo a aguardar
-                milissegundo: milissegundo a aguardar
-        Registradores utilizados: r0, r1, r7
-                obs:    r0 carrega o valor em s
-                        r1 carrega o valor em ms
+        Utiliza a macro GPIODirectionOut presente no
+        map.s para chamar a saida de cada pino do display
   ------------------------------------------------------*/
-.macro nanoSleep milissegundo
-        ldr R0,=timespecsec     @adiciona o valor da variavel second
-        ldr R1,=\milissegundo   @paramentro da macro
-        mov R7, #nano_sleep
-        swi 0
+.macro  setOut
+        GPIODirectionOut pinE
+        GPIODirectionOut pinRS
+        GPIODirectionOut pinDB7
+        GPIODirectionOut pinDB6
+        GPIODirectionOut pinDB5
+        GPIODirectionOut pinDB4
+        .ltorg
 .endm
 
 /*======================================================
-        Analisa qual botao foi precionado
+        Ativa ou desativa os pinos do display LCD
+        Recebe o nivel logico de cada uma das entradas 
+        dos pinos com excecao do enable
   ======================================================
-        Entradas:  
-                pin: pino a ser acessado
-        Registradores utilizados: r0, r2, r3, r8
-                obs:    r2 carrega ...
-                        r3 carrega o pino
+        Macros utilizadas: 
+                GPIOValue: presente no map.s, utilizada 
+                para setar um determinado dado no display.
+        Entradas: 
+                RS, DB7, DB6, DB5, DB4
   ------------------------------------------------------*/
-.macro GPIOReadRegister pin
-        mov r2, r8      @ Endereço dos registradores da GPIO
-        add r2, #level  @ offset para acessar o registrador do pin level 0x34 
-        ldr r2, [r2]    @ pino5, 19 e 26
-        ldr r3, =\pin  
-        add r3, #8      @ offset para acessar a terceira word
-        ldr r3, [r3]    /* carrega a posiçao do pino -> 
-                          ex queremos saber o valor do pino5 =2^5= 32 => 00 000 000 000 000 000 000 000 000 100 000*/
-        and r0, r2, r3  @ Filtrando os outros bits => 00 000 000 000 000 000 000 000 000 100 000
+.macro setDisplay RS, DB7, DB6, DB5, DB4
+        GPIOValue pinE, #0
+        GPIOValue pinRS, #\RS
+        GPIOValue pinE, #1
+        GPIOValue pinDB7, #\DB7
+        GPIOValue pinDB6, #\DB6
+        GPIOValue pinDB5, #\DB5
+        GPIOValue pinDB4, #\DB4
+        GPIOValue pinE, #0
 .endm
 
 /*======================================================
-        Direciona a saida de um dado pino
+        Inicializa o display seguindo as orientacoes do 
+        datashit
   ======================================================
-        Entradas:
-                pin: pino a ser direcionado
-        Registradores utilizados: r0, r1, r2, r3, r8
+        Macros utilizadas:  
+                setDisplay: usada para setar um
+                determinado dado no display
+                nanoSleep: presente no map.s, utilizada
+                para aguardar um determinado tempo
   ------------------------------------------------------*/
-.macro GPIODirectionOut pin
-        ldr r2, =\pin
-        ldr r2, [r2]
-        ldr r1, [r8, r2]
-        ldr r3, =\pin   @ address of pin table
-        add r3, #4      @ load amount to shift from table
-        ldr r3, [r3]    @ load value of shift amt
-        mov r0, #0b111  @ mask to clear 3 bits
-        lsl r0, r3      @ shift into position
-        bic r1, r0      @ clear the three bits
-        mov r0, #1      @ 1 bit to shift into pos
-        lsl r0, r3      @ shift by amount from table
-        orr r1, r0      @ set the bit
-        str r1, [r8, r2] @ save it to reg to do work
+.macro init
+        setDisplay 0, 0, 0, 1, 1  
+        nanoSleep timespecnano5
+
+        setDisplay 0, 0, 0, 1, 1 
+        nanoSleep timespecnano150  
+
+        setDisplay  0, 0, 0, 1, 1
+
+
+        setDisplay 0, 0, 0, 1, 0
+        nanoSleep timespecnano150  
+
+        .ltorg 
+
+        setDisplay 0, 0, 0, 1, 0 
+
+        setDisplay 0, 0, 0, 0, 0
+        nanoSleep timespecnano150 
+        setDisplay 0, 0, 0, 0, 0 
+
+        setDisplay 0, 1, 0, 0, 0  
+        nanoSleep timespecnano150
+
+        setDisplay 0, 0, 0, 0, 0  
+
+        setDisplay 0, 0, 0, 0, 1  
+        nanoSleep timespecnano150 
+
+        setDisplay 0, 0, 0, 0, 0
+
+        setDisplay 0, 0, 1, 1, 0 
+        nanoSleep timespecnano150
+
+        .ltorg
 .endm
 
 /*======================================================
-        Seta um valor para um determinado pino
+        Realiza o deslocamento do cursor
   ======================================================
-        Entradas:
-                pin: pino que recebera a informacao
-                value: valor a ser enviado
-        Registradores utilizados: r0, r1, r2, r3 r5, r8
+        Macros utilizadas:  
+                setDisplay: usada para setar um
+                determinado dado no display
+                nanoSleep: presente no map.s, utilizada
+                para aguardar um determinado tempo
   ------------------------------------------------------*/
-.macro GPIOValue pin value
-        mov r0, #40     @ valor do clear off set
-        mov r2, #12     @ valor que ao subtrair o clear off set resulta 28 o set
-        mov r1, \value  @ registra o valor 0 ou 1 no registrador
-        mul r5, r1, r2  @ Ex r1 recebe o valor 1, ou seja multiplica o 12 do r2 por 1 resultando 12 no r5
-        sub r0, r0, r5  @ valor do r5 que é 12 subtraido por 40 do r0 resultando 28 para o r0 ou seja o set do offset
-        mov r2, r8      @ Endereço dos registradores da GPIO
-        add r2, r2, r0  @ adiciona no r2 o valor do set com o endereço dos regs
-        mov r0, #1      @ 1 bit para o shift
-        ldr r3, =\pin   @ valor dos endereços dos pinos
-        add r3, #8      @ Adiciona offset para shift 
-        ldr r3, [r3]    @ carrega o shift da tabela
-        lsl r0, r3      @ realiza a mudança
-        str r0, [r2]    @ Escreve no registro
-.endm 
+.macro entryModSetMacro
+        setDisplay 0, 0, 0, 0, 0
+        setDisplay 0, 1, 1, 1, 0
+        nanoSleep timespecnano150
 
-/*======================================================
-        Realiza o mapeamento dos pinos do display
-  ======================================================
-        Registradores utilizados: r0, r1, r2, r3 r4, r5, 
-                                  r7, r8
-  ------------------------------------------------------*/
-.macro map
-        ldr r0, =fileName
-	mov r1, #0x1b0
-	orr r1, #0x006
-	mov r2, r1
-	mov r7, #sys_open
-	swi 0
-	movs r4, r0
-
-	ldr r5, =gpioaddr
-	ldr r5, [r5]
-	mov r1, #pagelen
-	mov r2, #(prot_read + prot_write)
-	mov r3, #map_shared
-	mov r0, #0
-	mov r7, #sys_map
-	swi 0
-	movs r8, r0
+        setDisplay 0, 0, 0, 0, 0
+        setDisplay 0, 0, 1, 1, 0
+        nanoSleep timespecnano150
+        .ltorg
 .endm
 
 /*======================================================
-        Realiza a entrada de dados na tela (input)
+        Limpa o display
   ======================================================
-        Registradores utilizados: r0, r1, r2, r7
-                obs: r1 carrega o variavel
+        Macros utilizadas:  
+                GPIOsetDisplay: usada para setar um
+                determinado dado no display
+                nanoSleep: presente no map.s, utilizada
+                para aguardar um determinado tempo
   ------------------------------------------------------*/
-.macro input variavel, len_variavel
-        mov r0, #1          @ atribui 1 ao r0 para escrever na tela
-        ldr r1, =\variavel      @ dado para impressao
-        mov r2, #\len_variavel  @ tamanho da palavra a ser exidido
-        mov r7, #3          @ chamada de sistema para escrita
-        swi 0
-.endm
+clearDisplay:
+        setDisplay 0, 0, 0, 0, 0
+        setDisplay 0, 0, 0, 0, 1
+        nanoSleep timespecnano150 
+bx lr
 
 /*======================================================
-        Realiza a impressao na tela (print)
+        Realiza o deslocamento do cursor
   ======================================================
-        Entradas:
-                variavel -> variavel a ser exibido na tela
-                len_variavel -> tamanho do variavel
-        Registradores utilizados: r0, r1, r2, r7
+        Macros utilizadas:  
+                setDisplay: usada para setar um
+                determinado dado no display
+                nanoSleep: presente no map.s, utilizada
+                para aguardar um determinado tempo
   ------------------------------------------------------*/
-.macro print variavel, len_variavel
-        mov r0, #1          @ atribui 1 ao r0 para escrever na tela
-        ldr r1, =\variavel      @ dado para impressao
-        mov r2, #\len_variavel  @ tamanho da palavra a ser exidido
-        mov r7, #4          @ chamada de sistema para escrita
-        swi 0 
-.endm
+entryModeSet:
+        setDisplay 0, 0, 0, 0, 0
+        setDisplay 0, 1, 1, 1, 0
+        nanoSleep timespecnano150
+
+        setDisplay 0, 0, 0, 0, 0
+        setDisplay 0, 0, 1, 1, 0
+        nanoSleep timespecnano150
+        .ltorg
+bx lr
+
+/*======================================================
+        Inicializa o display e faz o mapeamento dos pinos
+  ======================================================
+        Macros utilizadas:  
+                entryModeset
+                init
+                setOut
+
+                map: Realiza o mapeamento
+  ------------------------------------------------------*/
+initDisplay:
+        map
+        setOut
+        init
+        entryModSetMacro
+bx lr
+
+/*======================================================
+        Realiza a exibicao de uma cadeia de caracteres 
+        no display
+  ======================================================
+        Macros utilizadas:  
+                GPIOValue: usada para setar cada bit que
+                representa o caractere
+                nanoSleep: presente no map.s, utilizada
+                para aguardar um determinado tempo
+  ------------------------------------------------------*/
+write_lcd:
+        mov r9, r0             @ passa o valor do texto para r10
+        mov r6, #7
+        mov r10, #256
+        loop_bit:                @ percorre todos os 8 bits do bit para sabe o nivel logico
+                lsr r10, #1      @ desloca o bit para a direita  ex: 100000000 -> 010000000
+                and r4, r10, r9  @ faz um and entre r10 e r9 para saber se o bit esta ativo ou nao
+                cmp r4, #0
+                beq switch 	 @ se for igual a 0 valor nao sera alterado          
+                mov r4, #1
+                switch:
+                        @ se for 0 seta no pino DB4
+                        cmp r6, #0
+                        beq case4
+                        cmp r6, #4
+                        beq case4
+                        @ se for 1 seta no pino DB5
+                        cmp r6, #1
+                        beq case3
+                        cmp r6, #5
+                        beq case3
+                        @ se for 2 seta no pino DB6
+                        cmp r6, #2
+                        beq case2
+                        cmp r6, #6
+                        beq case2
+                        @ se for 3 seta no pino DB7
+                        cmp r6, #3
+                        beq case1
+                        cmp r6, #7
+                        beq case1
+
+                        case1:
+                                GPIOValue pinE, #0 @ atribui 0 ao enable
+                               	GPIOValue pinRS, #1
+                                GPIOValue pinE, #1
+                                GPIOValue pinDB7, r4
+                                b retornar @ pula os outros casos
+                        case2:
+                                GPIOValue pinDB6, r4
+                                b retornar  @ pula os outros casos
+                        case3:
+                                GPIOValue pinDB5, r4
+                                b retornar @ pula os outros casos
+                        case4:
+                                GPIOValue pinDB4, r4
+                                GPIOValue pinE, #0
+                                @enable
+                retornar:
+                        sub r6, #1       @ subtrai +1 a r0
+                        cmp r10, #1      @ compara o valor de r0 para saber se ja percorreu o ultimo bit
+        bne loop_bit
+	
+bx lr
+
+@ variaveis utilizadas no codigo
+.data
+	timespecsec: .word 0 @definição do nano sleep 0s permitindo os milissegundos
+	timespecnano5: .word 5000000 @valor em milisegundos para lcd
+	timespecnano150: .word 150000 @valor em milisegundos para LCD
+
+	fileName: .asciz "/dev/mem"
+	gpioaddr: .word 0x20200 @carrega o endereco os onde registradores do controlador GPIO são mapeados na memória
+
+        @ pino do LED
+        pin6:   .word 0
+                .word 18
+                .word 6
+
+        @ pinos dos botoes
+        pin19:  .word 4
+                .word 27
+                .word 524288
+        pin26:  .word 8
+                .word 18
+                .word 67108864 
+
+        @ pinos do display LCD
+        pinRS:	@ Pino RS - GPIO25
+                .word 8
+                .word 15
+                .word 25
+	pinE:	@ Pino Enable - GPIO1
+		.word 0
+		.word 3
+		.word 1
+	pinDB4: @ Pino DB4 - GPIO12
+		.word 4
+		.word 6
+		.word 12
+	pinDB5: @ Pino DB5 - GPIO16
+		.word 4
+		.word 18
+		.word 16
+	pinDB6: @ Pino DB6 - GPIO20
+		.word 8
+		.word 0
+		.word 20
+	pinDB7: @ Pino DB7 - GPIO21
+		.word 8
+		.word 3
+		.word 21
